b556b29a2fb28122e2b49c88c0de3836c2758b97
Haxosura <123653087+Haxosura@users.noreply.github.com>
2024-02-12 09:28:41 +0000

    Move and First Code

--- Source/Gameplay_Project/EnemyCharacter.cpp
+++ Source/Gameplay_Project/EnemyCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EnemyCharacter.h"
+#include "Gameplay_ProjectCharacter.h"
 
 // Called when the game starts or when spawned
 void AEnemyCharacter::BeginPlay()
@@ -44,7 +44,7 @@ APatrolPath* AEnemyCharacter::GetPatrolPath() const
 void AEnemyCharacter::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (auto* const ch = Cast<AAPI_ProjectCharacter>(OtherActor))
+	if (auto* const ch = Cast<AGameplay_ProjectCharacter>(OtherActor))
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Purple, TEXT("Actor Hit"));
 	}
--- Source/Gameplay_Project/EnemyCharacter.h
+++ Source/Gameplay_Project/EnemyCharacter.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "PatrolPath.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Components/SphereComponent.h"
 #include "EnemyCharacter.generated.h"
 
 UCLASS()
